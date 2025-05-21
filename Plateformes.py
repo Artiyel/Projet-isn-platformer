26488df5c56ec7b6d95ddde2228c55b3cb05b2fa
charl0tttt <charlotte.cp05@gmail.com>
2025-05-21 15:27:33 +0200

    commentaires

--- Plateformes.py
+++ Plateformes.py
@@ -2,7 +2,8 @@ from Elements_Decor import ElementDecor
 
 class Plateforme(ElementDecor):
     """
-
+    classe des plateformes
+    hérite de la classe ElementDecor
     """
     def __init__(self, x_pos, y_pos, x_taille = 100, y_taille = 10):
         super().__init__(x_pos, y_pos, x_taille, y_taille)
