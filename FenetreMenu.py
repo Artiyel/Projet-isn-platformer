ef04a79e21ecc396571ba131315b2664e1bbb21f
charl0tttt <charlotte.cp05@gmail.com>
2025-06-01 17:45:35 +0200

    une fenetre menu pas vide ??

--- FenetreMenu.py
+++ FenetreMenu.py
@@ -2,7 +2,7 @@ import pygame
 from Fenetre import Fenetre
 
 class FenetreMenu(Fenetre):
-    def __init__(self, background = pygame.image.load("assets/fond_1.png"), buttons = []):
+    def __init__(self, background = pygame.image.load("assets/fond_menu_1.png"), buttons = []):
         super(FenetreMenu,self).__init__(background)
         self.buttons = buttons
         print("Taille de la fenêtre :", self.window.get_width(), "x", self.window.get_height())
@@ -17,10 +17,12 @@ class FenetreMenu(Fenetre):
         # super().draw()
         # #on affiche les boutons
         # self.draw_buttons()   
-        self.window.fill((30, 30, 60))  # fond bleu foncé
-        font = pygame.font.SysFont("Arial", 48)
-        titre = font.render("Plateformer ISN", True, (255, 255, 255))
-        self.window.blit(titre, (100, 50))
+
+        font = pygame.font.SysFont("Chiller", 200, bold=True)
+        titre = font.render("ICI", True, (255, 167, 17))
+        text_rect = titre.get_rect(center=(self.window.get_width() // 2, 250))
+        self.window.blit(titre, text_rect)
+
         # Affiche les boutons
         for bouton in self.buttons:
             bouton.draw(self.window)
